29154aaad9d43f80522c17dc9c7d9fa6ad5a0267
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-03-02 21:08:28 +0100

    intentando crear volumenes desde el compose

---
+++ sql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS tabla_prueba2 (
+    id2 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO prueba_tabla2 (name) values ("antonio");
