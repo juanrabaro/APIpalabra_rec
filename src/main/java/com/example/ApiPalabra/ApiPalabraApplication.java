0bb91f95f085cf6908afbb541bf199120daf883e
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-03-02 16:18:48 +0100

    subido inicializador

---
+++ src/main/java/com/example/ApiPalabra/ApiPalabraApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiPalabra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPalabraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPalabraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiPalabra/ApiPalabraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiPalabra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPalabraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
