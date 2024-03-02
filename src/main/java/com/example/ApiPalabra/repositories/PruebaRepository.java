0582d33ce82eb8befea87679be1f7d37544ffda0
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-03-02 18:40:10 +0100

    no funciona

---
+++ src/main/java/com/example/ApiPalabra/repositories/PruebaRepository.java
@@ -0,0 +1,7 @@
+package com.example.ApiPalabra.repositories;
+
+import com.example.ApiPalabra.models.PruebaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PruebaRepository extends JpaRepository<PruebaModel, Long> {
+}
