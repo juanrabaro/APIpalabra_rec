cf65e2a880aeda2013e16fbabfef69d1592668c5
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-03-04 09:44:35 +0100

    post y delete de game-match listo

--- src/main/java/com/example/ApiPalabraRec/Controllers/GameMatchesController.java
+++ src/main/java/com/example/ApiPalabraRec/Controllers/GameMatchesController.java
@@ -3,9 +3,7 @@ package com.example.ApiPalabraRec.Controllers;
 import com.example.ApiPalabraRec.Models.GameMatchesModel;
 import com.example.ApiPalabraRec.Services.GameMatchesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,15 @@ public class GameMatchesController {
     @GetMapping("/game-matches")
     public List<GameMatchesModel> getGameMatches() {
         return gameMatchesService.getAllGameMatches();
+    }
+
+    @PostMapping("/game-matches")
+    public GameMatchesModel createGameMatch(@RequestBody GameMatchesModel gameMatch) {
+        return gameMatchesService.createGameMatch(gameMatch);
+    }
+
+    @DeleteMapping("/game-matches/{id}")
+    public String deleteGameMatch(@PathVariable Integer id) {
+        return gameMatchesService.deleteGameMatch(id);
     }
 }
--- src/main/java/com/example/ApiPalabraRec/Services/GameMatchesService.java
+++ src/main/java/com/example/ApiPalabraRec/Services/GameMatchesService.java
@@ -1,11 +1,17 @@
 package com.example.ApiPalabraRec.Services;
 
 import com.example.ApiPalabraRec.Models.GameMatchesModel;
+import com.example.ApiPalabraRec.Models.GamesModel;
+import com.example.ApiPalabraRec.Models.PlayersModel;
 import com.example.ApiPalabraRec.Repositories.IGameMatchesRepository;
+import com.example.ApiPalabraRec.Repositories.IGamesRepository;
+import com.example.ApiPalabraRec.Repositories.IPlayersRepository;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameMatchesService {
@@ -13,7 +19,43 @@ public class GameMatchesService {
     @Autowired
     private IGameMatchesRepository igameMatchesRepository;
 
+    @Autowired
+    private IGamesRepository igamesRepository;
+
+    @Autowired
+    private IPlayersRepository iplayersRepository;
+
     public List<GameMatchesModel> getAllGameMatches() {
         return igameMatchesRepository.findAll();
+    }
+
+    public GameMatchesModel createGameMatch(GameMatchesModel gameMatch) {
+        System.out.println(gameMatch);
+        if (gameMatch.getGame() == null) {
+            throw new RuntimeException("El juego del match no puede ser nulo");
+        }
+        GamesModel existingGame = igamesRepository.findById(gameMatch.getGame().getId_game()).orElse(null);
+        if (existingGame == null) {
+            throw new RuntimeException("El juego no existe");
+        }
+
+        if (gameMatch.getPlayer() == null) {
+            throw new RuntimeException("El jugador no puede ser nulo");
+        }
+        PlayersModel existingPlayer = iplayersRepository.findById(gameMatch.getPlayer().getId_player()).orElse(null);
+        if (existingPlayer == null) {
+            throw new RuntimeException("El jugador no existe");
+        }
+
+        return igameMatchesRepository.save(gameMatch);
+    }
+
+    public String deleteGameMatch(Integer id) {
+        GameMatchesModel gameMatch = igameMatchesRepository.findById(id).orElse(null);
+        if (gameMatch == null) {
+            throw new RuntimeException("El match no existe");
+        }
+        igameMatchesRepository.deleteById(id);
+        return "El game match con el id " + id +" ha sido eliminado";
     }
 }
