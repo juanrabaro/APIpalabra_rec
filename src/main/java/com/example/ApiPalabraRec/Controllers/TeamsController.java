1a795edd91005e00c9ba0ff0e5dc52a052fc1f61
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-03-04 08:32:08 +0100

    delete teams funciona borra todos los players del team y los game matches del player

--- src/main/java/com/example/ApiPalabraRec/Controllers/TeamsController.java
+++ src/main/java/com/example/ApiPalabraRec/Controllers/TeamsController.java
@@ -3,9 +3,7 @@ package com.example.ApiPalabraRec.Controllers;
 import com.example.ApiPalabraRec.Models.TeamsModel;
 import com.example.ApiPalabraRec.Services.TeamsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class TeamsController {
     @GetMapping("/teams")
     public List<TeamsModel> getAllTeams() {
         return teamsService.getAllTeams();
+    }
+
+    @DeleteMapping("/teams/{id}")
+    public String deleteTeam(@PathVariable Integer id) {
+        return teamsService.deleteTeam(id);
     }
 }
--- src/main/java/com/example/ApiPalabraRec/Services/TeamsService.java
+++ src/main/java/com/example/ApiPalabraRec/Services/TeamsService.java
@@ -1,6 +1,10 @@
 package com.example.ApiPalabraRec.Services;
 
+import com.example.ApiPalabraRec.Models.GameMatchesModel;
+import com.example.ApiPalabraRec.Models.PlayersModel;
 import com.example.ApiPalabraRec.Models.TeamsModel;
+import com.example.ApiPalabraRec.Repositories.IGameMatchesRepository;
+import com.example.ApiPalabraRec.Repositories.IPlayersRepository;
 import com.example.ApiPalabraRec.Repositories.ITeamsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +18,13 @@ public class TeamsService {
     @Autowired
     private ITeamsRepository iTeamsRepository;
 
+    @Autowired
+    private IPlayersRepository iPlayersRepository;
+
+    @Autowired
+    private IGameMatchesRepository iGameMatchesRepository;
+
+
     public List<TeamsModel> getAllTeams() {
         return iTeamsRepository.findAll();
     }
@@ -24,5 +35,29 @@ public class TeamsService {
 
     public TeamsModel saveTeam(TeamsModel team) {
         return iTeamsRepository.save(team);
+    }
+
+    public String deleteTeam(Integer id) {
+        TeamsModel team = iTeamsRepository.findById(id).orElse(null);
+        if (team == null) {
+            throw new RuntimeException("El equipo no existe");
+        }
+
+        List<GameMatchesModel> gameMatches = iGameMatchesRepository.findAll();
+        for (GameMatchesModel gameMatch : gameMatches) {
+            if (gameMatch.getPlayer().getId_player() == id) {
+                iGameMatchesRepository.deleteById(gameMatch.getId_match());
+            }
+        }
+
+        List<PlayersModel> players = iPlayersRepository.findAll();
+        for (PlayersModel player : players) {
+            if (player.getTeam().getId_team() == id) {
+                iPlayersRepository.deleteById(player.getId_player());
+            }
+        }
+
+        iTeamsRepository.deleteById(id);
+        return "Equipo con id " + id + " eliminado";
     }
 }
