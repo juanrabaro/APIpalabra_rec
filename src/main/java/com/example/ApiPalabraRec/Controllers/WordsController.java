066c689bc86ed7a2c3180d299215a8781c87009e
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-03-04 10:06:15 +0100

    get palabras aleatorias listo

--- src/main/java/com/example/ApiPalabraRec/Controllers/WordsController.java
+++ src/main/java/com/example/ApiPalabraRec/Controllers/WordsController.java
@@ -4,6 +4,7 @@ import com.example.ApiPalabraRec.Models.WordsModel;
 import com.example.ApiPalabraRec.Services.WordsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class WordsController {
     @GetMapping("/words")
     public List<WordsModel> getAllWords() {
         return wordsService.getAllWords();
+    }
+
+    @GetMapping("/words/random-words/{nWords}")
+    public List<WordsModel> getRandomWords(@PathVariable int nWords) {
+        return wordsService.getRandomWords(nWords);
     }
 }
--- src/main/java/com/example/ApiPalabraRec/Services/WordsService.java
+++ src/main/java/com/example/ApiPalabraRec/Services/WordsService.java
@@ -5,6 +5,7 @@ import com.example.ApiPalabraRec.Repositories.IWordsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -15,5 +16,13 @@ public class WordsService {
 
     public List<WordsModel> getAllWords() {
         return iWordsRepository.findAll();
+    }
+
+    public List<WordsModel> getRandomWords(int nWords) {
+        List<WordsModel> allWords = iWordsRepository.findAll();
+
+        Collections.shuffle(allWords);
+
+        return allWords.subList(0, Math.min(nWords, allWords.size()));
     }
 }
